faf3538fcf6647a5a919201a9aa5e65cf8c6db3c
huuautomation <116048173+huuautomation@users.noreply.github.com>
2022-11-01 23:49:43 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/fireb
 const firebaseConfig = {
 
   //Điền thông tin Firebase config của cá nhân vào đây
-  Mai Trọng Hữu 
-  B1913028
-  KC326-Iot
+  apiKey : "AIzaSyAoQewRqtcwQl70aH24xCPTowSAzW8iI2Q" , 
+  authDomain : "kc326-test.firebaseapp.com" , 
+  databaseURL : "https://kc326-test-default-rtdb.asia-southeast1.firebaseddatabase.app" , 
+  projectId : "kc326-test" , 
+  storageBucket : "kc326-test.appspot.com" , 
+  messagingSenderId : "9498157636" , 
+  appId : "1:9498157636:web:6e8a840974247b2ece1211" 
 };
 
 // Initialize Firebase
